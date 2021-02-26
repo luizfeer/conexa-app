34062809aca1a7f8d7c24d771214093228fb7249
Gabriel Rocha <gabriel.rocha@conexasaude.com.br>
2021-02-26 17:41:22 -0300

    finishing mock backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
+const path = require('path')
 const jsonServer = require('json-server')
+const { login, authMiddleware } = require(path.join(__dirname, './Handlers/auth'))
 
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(jsonServer.bodyParser)
+
+server.post('/login', login)
+
+server.use(authMiddleware)
+
 server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running<>>', 'http://localhost:3000')
+server.listen(process.env.PORT || 3333, () => {
+  console.log('JSON Server is running<>>', 'http://localhost:3333')
 })
